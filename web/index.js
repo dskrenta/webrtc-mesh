c31d7a912a095e757b4931737c9b432a299f7fd0
dskrenta <dskrenta@gmail.com>
2020-03-28 20:05:41 -0700

    basic structure on client

--- web/index.js
+++ web/index.js
@@ -1,3 +1,25 @@
 (() => {
   'use strict';
+
+  async function main() {
+    try {
+      const formElement = document.getElementById('inputForm');
+      const usernameInputElement = document.getElementById('username');
+      const roomIdInputElement = document.getElementById('roomId');
+
+      formElement.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const username = usernameInputElement.value;
+        const roomId= roomIdInputElement.value;
+
+        console.log(username, roomId);
+      });
+    }
+    catch (error) {
+      console.error('main error', error);
+    }
+  }
+
+  main();
 })();
